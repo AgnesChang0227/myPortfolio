14b14691d0dc67da0392ccc0232bf457b93dc28f
AgnesChang0227 <109337309+AgnesChang0227@users.noreply.github.com>
2022-10-30 15:02:21 +0800

    try the src in deploy and dev

--- src/config/project.js
+++ src/config/project.js
@@ -7,7 +7,7 @@ export const projects = [
         url: {
             PPT: "https://docs.google.com/presentation/d/1_ZMCcE3xZAcvHe5PYwRL_0gxGhtc8H-4/edit?usp=sharing&ouid=103558104135438308829&rtpof=true&sd=true",
         },
-        capture: "./images/owlSchool",//it is a folder
+        captureFolder: "images/owlSchool",//it is a folder
         frontend: "React, react-router-dom, tailwind, axios...",
         backend: "Node.js, express.js, jwt, bcrypt,mongoose... ",
         database: "MongoDB",
--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -1,46 +1,30 @@
 import React from 'react';
 
+
 const ProjectList = () => {
+    const reader = new FileReader();
+    console.log()
     return (
         <div>
             <div className="row">
-                <div className="col-2 bg-dark position-sticky top-0" style={{height:"100vh",paddingTop:"70px"}} >
+                <div className="col-2 bg-dark position-sticky top-0" style={{height: "100vh", paddingTop: "70px"}}>
                     <div id="simple-list-example"
                          className="d-flex flex-column gap-2 simple-list-example-scrollspy">
-
-                        <a className=" w-100 rounded btn btn-secondary btn-sm" href="#simple-list-item-1">Item 1</a>
-                    </div>
+                        <a className=" w-100 rounded btn btn-secondary btn-sm" href="/myPortfolio#/projects/#simple-list-item-1">Item 1</a>
+\                    </div>
                 </div>
-                <div className="col-10" >
-                    <div className="scrollspy-example justify-content-center"
-                        data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0"
-                         data-bs-smooth-scroll="true"  tabIndex="0">
-                        <div className="card mb-3 w-75 bg-dark text-white background-image: var(--bs-gradient)" id="simple-list-item-1">
-                            <img src={`./${process.env.REACT_APP_REPO ||''}images/owlSchool.png`}
+                <div className="col-10">
+                    <div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0"
+                         data-bs-smooth-scroll="true" className="scrollspy-example justify-center" tabIndex="0">
+                        <div className="card mb-3 w-75 bg-dark text-white background-image: var(--bs-gradient)"
+                             id="simple-list-item-1">
+                            {/*<img  src={`./${process.env.REACT_APP_REPO ||''}${project.captureFolder}/homepage.png`}*/}
+                            <img  src={`./${process.env.REACT_APP_REPO ||''}images/owlSchool.png`}
                                  className="card-img-top" alt="..."/>
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                        <br/>
-                        <div className="card mb-3 w-75 bg-dark text-white background-image: var(--bs-gradient)" id="simple-list-item-1">
-                            <img src={`./${process.env.REACT_APP_REPO ||''}images/owlSchool.png`}
-                                 className="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
-                                    additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                        <br/>
-                        <div className="card mb-3 w-75 bg-dark text-white background-image: var(--bs-gradient)" id="simple-list-item-1">
-                            <img src={`./${process.env.REACT_APP_REPO ||''}images/owlSchool.png`}
-                                 className="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                <p className="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to
                                     additional content. This content is a little bit longer.</p>
                             </div>
                         </div>
