609ee3ea82d9189f4fa2ef73904bc7930d81b42c
AgnesChang0227 <109337309+AgnesChang0227@users.noreply.github.com>
2022-10-30 13:47:55 +0800

    try the src in deploy and dev

--- src/config/project.js
+++ src/config/project.js
@@ -1,14 +1,16 @@
-export const projects=[
+export const projects = [
     {
-        title:"OwlSchool",
-        description:"It's a MERN stack",
-        url:{
-            PPT:"https://docs.google.com/presentation/d/1_ZMCcE3xZAcvHe5PYwRL_0gxGhtc8H-4/edit?usp=sharing&ouid=103558104135438308829&rtpof=true&sd=true",
+        title: "OwlSchool",
+        description: ["An Udemy like online learning website.",
+        "Including role: visitor, student, teacher, admin.",
+        "Functions : Authentication, Send Email, Create / Edit / Enroll Courses, Upload Image/ Video , Edit Profile..."],
+        url: {
+            PPT: "https://docs.google.com/presentation/d/1_ZMCcE3xZAcvHe5PYwRL_0gxGhtc8H-4/edit?usp=sharing&ouid=103558104135438308829&rtpof=true&sd=true",
         },
-        capture:"./myPortfolio/images/owlSchool",//it is a folder
-        frontend:"React, react-router-dom, tailwind, axios...",
-        backend:"Node.js, express.js, jwt, bcrypt,mongoose... ",
-        database:"MongoDB",
-        finishedDate:new Date("2022-10-21")
+        capture: "./images/owlSchool",//it is a folder
+        frontend: "React, react-router-dom, tailwind, axios...",
+        backend: "Node.js, express.js, jwt, bcrypt,mongoose... ",
+        database: "MongoDB",
+        finishedDate: new Date("2022-10-21")
     }
 ]
