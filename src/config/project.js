65d44b85c077627effaef827f6040fce90d3b3e9
AgnesChang0227 <109337309+AgnesChang0227@users.noreply.github.com>
2022-10-30 23:35:10 +0800

    try

--- src/config/project.js
+++ src/config/project.js
@@ -1,16 +1,57 @@
 export const projects = [
     {
-        title: "OwlSchool",
+        title: "Owl School",
         description: ["An Udemy like online learning website.",
-        "Including role: visitor, student, teacher, admin.",
-        "Functions : Authentication, Send Email, Create / Edit / Enroll Courses, Upload Image/ Video , Edit Profile..."],
+            "Including role: visitor, student, teacher, admin.",
+            "Functions : Authentication, Send Email, Create / Edit / Enroll Courses, Upload Image/ Video , Edit Profile..."
+        ],
         url: {
             PPT: "https://docs.google.com/presentation/d/1_ZMCcE3xZAcvHe5PYwRL_0gxGhtc8H-4/edit?usp=sharing&ouid=103558104135438308829&rtpof=true&sd=true",
         },
-        captureFolder: "images/owlSchool",//it is a folder
-        frontend: "React, react-router-dom, tailwind, axios...",
-        backend: "Node.js, express.js, jwt, bcrypt,mongoose... ",
-        database: "MongoDB",
+        captureFolder: "images/owlSchool/",//it is a folder
+        language: ["NodeJS", "ExpressJS", "ReactJS", "MongoDB"],
         finishedDate: new Date("2022-10-21")
-    }
-]
+    },
+    {
+        title: "Idea Note",
+        description: ["Using template engine EJS and Bootstrap to handle pages' appearance.",
+            "Using session and cookie to handle the Authentication.",
+            "Function:Register, Login, Logout, Add / Edit Note, Upload / Delete Profile Image."
+        ],
+        captureFolder: "images/ideaNote",//it is a folder
+        language: ["NodeJS", "ExpressJS", "EJS", "MongoDB"],
+        finishedDate: new Date("2022-9-27")
+    },
+    {
+        title: "Photo Website",
+        description: ["Show pages with React component and Bootstrap CSS.",
+            "Fetch external API to get photos.",
+            "Show quantity of related photos which depends on the pagination and LoadMore button.",
+            "Functions: Show Featured Image, Search and show related photos, Load more related photos."
+        ],
+        captureFolder: "images/photoWebsite",//it is a folder
+        language: ["ReactJS"],
+        finishedDate: new Date("2022-9-24")
+    },
+    {
+        title: "Learning System",
+        description: ["Show pages with React component and Bootstrap CSS.",
+            "Authentication using passport-jwt and passport-local",
+            "Function: Register, Login, Logout, Search courses by title, enroll Course, Create course"
+        ],
+        captureFolder: "images/learningSystem",//it is a folder
+        language: ["NodeJS", "ExpressJS", "ReactJS", "MongoDB"],
+        finishedDate: new Date("2022-9-28")
+    },
+    {
+        title: "Google and Local Login System",
+        description: ["Show pages with React component and Bootstrap CSS.",
+            "Authentication using passport-jwt and passport-local",
+            "Function: Register, Login, Logout, Search courses by title, enroll Course, Create course"
+        ],
+        captureFolder: "images/googleLogin",//it is a folder
+        language: ["NodeJS", "ExpressJS", "EJS", "MongoDB"],
+        finishedDate: new Date("2022-9-23")
+    },
+
+];
--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -1,34 +1,59 @@
-import React from 'react';
-
+import React, {useRef} from 'react';
+import {projects} from "../config/project";
+import {Link, useNavigate} from "react-router-dom";
 
 const ProjectList = () => {
-    const reader = new FileReader();
-    console.log()
+    const navigate = useNavigate();
+    const top = useRef(null);
+
+    const handleClick = (ref)=>{
+        ref.current?.scrollIntoView({behavior:"smooth",block:"center"});
+    }
+
     return (
         <div>
-            <div className="row">
+            <div className="row ">
                 <div className="col-2 bg-dark position-sticky top-0" style={{height: "100vh", paddingTop: "70px"}}>
                     <div id="simple-list-example"
                          className="d-flex flex-column gap-2 simple-list-example-scrollspy">
-                        <a className=" w-100 rounded btn btn-secondary btn-sm" href="/myPortfolio#/projects/#simple-list-item-1">Item 1</a>
+                        <a className=" w-100 rounded btn btn-secondary " onClick={()=>{handleClick(top)}}>Back to Top</a>
+                        {projects&&projects.sort((a,b)=> {
+                            return  b.finishedDate - a.finishedDate
+                        }).map((project,index)=>(
+                            <a className=" w-100 rounded btn btn-dark btn-sm border border-secondary" >{project.title}</a>
+                        ))}
 \                    </div>
                 </div>
-                <div className="col-10">
+                <div className="col-10 p-3" >
                     <div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0"
-                         data-bs-smooth-scroll="true" className="scrollspy-example justify-center" tabIndex="0">
-                        <div className="card mb-3 w-75 bg-dark text-white background-image: var(--bs-gradient)"
-                             id="simple-list-item-1">
-                            {/*<img  src={`./${process.env.REACT_APP_REPO ||''}${project.captureFolder}/homepage.png`}*/}
-                            <img  src={`./${process.env.REACT_APP_REPO ||''}images/owlSchool.png`}
-                                 className="card-img-top" alt="..."/>
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
-                            </div>
-                        </div>
-                        <br/>
+                         data-bs-smooth-scroll="true" className="scrollspy-example" tabIndex="0">
+                        <i ref={top}></i>
+                        {projects&&projects.sort((a,b)=> {
+                            return  b.finishedDate - a.finishedDate
+                        }).map((project,index)=>(
+                            <>
+                                <div className="card mb-3 bg-dark text-white background-image: var(--bs-gradient)"
+                                     style={{cursor:"pointer",width:"50vw"}}
+                                     id="simple-list-item-1" onClick={()=>{navigate("#")}}>
+                                    <img  src={`./${process.env.REACT_APP_REPO ||''}${project.captureFolder}/homepage.png`}
+                                          className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <p className="card-text" style={{color:"gray",float:"right"}}>
+                                            {project.finishedDate.toLocaleDateString()}
+                                        </p>
+                                        <h5 className="card-title">{project.title}</h5>
+                                        <h6 className="card-title">{project.language.join(" / ")}</h6>
+                                        <br/>
+                                        {project.description.map(item=>(
+                                            <div>
+                                                <li className="card-text">{item}</li>
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                                <br/>
+                            </>
+                        ))}
                     </div>
                 </div>
             </div>
