d4e26f280644b659341dc28530c2b06318af483f
AgnesChang0227 <109337309+AgnesChang0227@users.noreply.github.com>
2022-11-01 00:34:17 +0800

    change src for deploy

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,8 +5,8 @@ import Header from "../components/Header";
 const Homepage = () => {
     return (
         <>
-            <Header/>
-            <Slider/>
+            <div style={{backgroundImageL:"Url()"}}></div>
+            {/*<Header/>*/}
         </>
     );
 };
