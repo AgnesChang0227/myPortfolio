11759e4ce4a2a7e0996157f6e84b91eb457a0bd0
AgnesChang0227 <109337309+AgnesChang0227@users.noreply.github.com>
2024-02-20 17:44:25 +0800

    test img path

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React from "react";
+import icon from "../images/icon.jpg";
 
 const Header = () => {
-    return (
-        <div className="rounded-3 text-center p-3 mb-3"
-             style={{height: "auto", backgroundColor: "rgba(255,255,255,0.2)"}}>
-            <img src="./images/icon.jpg" className="rounded-circle"
-                 style={{height: "20vh"}}/>
-            <h1 style={{color: "#fff"}}>Agnes Chang</h1>
-            <h4 style={{color: "#fff"}}>Web developer</h4>
-            <h6 style={{color: "#fff"}}>
-                <i className="bi bi-envelope-check-fill" style={{fontSize: "16px"}}> </i>
-                agneschang0227@gmail.com
-            </h6>
-            <br/>
-            <div className="text-start p-3 text-white rounded-3"
-                 style={{height: "auto", backgroundColor: "rgba(0,0,0,0.2)"}}>
-                <h5>About Me</h5>
-                <p style={{marginBottom: "0px"}}>Hello, I'm Agnes. Welcome to my portfolio website! </p>
-                <p style={{marginBottom: "0px"}}>
-                    I was quit my clerk job in 2022 Feb and start learning different technology skiils.
-                    And I spent 2 months to self-learning in Java then take the course of backend web developter
-                    in ERB about 3 months.
-                </p>
-                <p style={{marginBottom: "0px"}}>Now I want to show you the projects I haven done or practise
-                    before.</p>
-                <p style={{marginBottom: "0px"}}>Welcome to send advice to me and thank you for your
-                    patience!</p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className="rounded-3 text-center p-3 mb-3"
+      style={{ height: "auto", backgroundColor: "rgba(255,255,255,0.2)" }}
+    >
+      <img
+        src={icon}
+        className="rounded-circle"
+        style={{ height: "20vh" }}
+      />
+      <h1 style={{ color: "#fff" }}>Agnes Chang</h1>
+      <h4 style={{ color: "#fff" }}>Web developer</h4>
+      <h6 style={{ color: "#fff" }}>
+        <i className="bi bi-envelope-check-fill" style={{ fontSize: "16px" }}>
+          {" "}
+        </i>
+        agneschang0227@gmail.com
+      </h6>
+      <br />
+      <div
+        className="text-start p-3 text-white rounded-3"
+        style={{ height: "auto", backgroundColor: "rgba(0,0,0,0.2)" }}
+      >
+        <h5>About Me</h5>
+        <p style={{ marginBottom: "0px" }}>
+          Hello, I'm Agnes. Welcome to my portfolio website!{" "}
+        </p>
+        <p style={{ marginBottom: "0px" }}>
+          I made the decision to leave my clerk job in February 2022 and venture
+          into a new path of acquiring diverse technology skills. With great
+          determination, I devoted two months to self-learning Java, laying a
+          solid foundation. Subsequently, I enrolled in a three-month web
+          development course at ERB, honing my skills further.
+        </p>
+        <p style={{ marginBottom: "0px" }}>
+          To provide you with a better understanding of my capabilities, I have
+          attached my previous project and resume. I hope these resources prove
+          to be helpful in assessing my abilities.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
